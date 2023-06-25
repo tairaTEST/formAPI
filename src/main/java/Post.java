0023f20192c3a2df7f8d933eb97ac83609fe0687
Tatiana Noskova <tnoskowa@mail.ru>
2023-06-25 19:00:38 +0500

    init

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String name; // поле для имени
+    post.name ="Иван";
+    post.passport ="4444 № 44444444";
+    post.patronymic ="Иванович";
+    post.phone ="+7 (999)-999-99-99";
+    post.surname ="Иванов";
+    post.subscription =true;
+}
